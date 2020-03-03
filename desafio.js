9f4df409c83f5f4056ab33cec30c639e1f49db61
jonatasdamascenoalves <61021433+jonatasdamascenoalves@users.noreply.github.com>
2020-03-03 17:00:22 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,24 @@
+function Click(){
+    alert("Olá")
+}
+function confirmacao(){
+   if (confirm("E ai tudo bem!!")){
+
+   }
+        
+}
+function chamar(){
+    var chamar = prompt('Manifeste sua satisfação com a fantasia do presidente vampiro:');
+}
+function troca(){
+     botaonome.textContent = "Olá "+prompt("Informe o seu nome por favor")+" tudo bem?"
+}
+function abrir(URL) {
+    window.open(URL, 'janela', 'width=795, height=590, top=100, left=699, scrollbars=no, status=no, toolbar=no, location=no, menubar=no, resizable=no, fullscreen=no')
+  }
+  
+  
+  function abrir2() {
+    URL="https://uol.com.br";
+    window.open(URL, 'janela', 'width=660, height=510, top=100, left=699, scrollbars=yes, status=no, toolbar=no, location=no, menubar=no, resizable=no, fullscreen=no')
+  }
